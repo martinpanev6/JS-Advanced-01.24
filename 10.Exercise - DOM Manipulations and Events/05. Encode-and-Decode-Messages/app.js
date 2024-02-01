91e14311c4600b525d967e74adb2ea88dc9998fd
martinpanev6 <145485188+martinpanev6@users.noreply.github.com>
2024-02-01 03:01:21 +0200

    Adding new exercises

--- 10.Exercise - DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
+++ 10.Exercise - DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,34 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    
+    const textAreaRefs = document.querySelectorAll("textarea");
+    const buttonsRef = document.querySelectorAll("button");
+    
+    const inputRef = textAreaRefs[0];
+    const outputRef = textAreaRefs[1];
+    const encodeButton = buttonsRef[0];
+    const decodeButton = buttonsRef[1];
+
+    encodeButton.addEventListener("click", encodeEventHandler);
+    decodeButton.addEventListener("click", decodeEventHandler);
+
+    function encodeEventHandler(e){
+        let message = inputRef.value;
+        let encodedMsg = "";
+        for(let char of message){
+            let valASCII = char.charCodeAt(0);
+            encodedMsg += String.fromCharCode(valASCII + 1);
+        }
+        inputRef.value = "";
+        outputRef.value = encodedMsg;
+    }
+
+    function decodeEventHandler(e){
+        let message = outputRef.value;
+        let decodedMsg = "";
+        for(let char of message){
+            let valASCII = char.charCodeAt(0);
+            decodedMsg += String.fromCharCode(valASCII - 1);
+        }
+        outputRef.value = decodedMsg
+    }
 }
