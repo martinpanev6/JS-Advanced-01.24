0b532fed1d9f90c7afe4b60e388b9be6e5933034
martinpanev6 <145485188+martinpanev6@users.noreply.github.com>
2024-02-01 02:34:39 +0200

    Adding new exercises

--- 9.DOM Manipulations and Events/05. Highlight-Active/app.js
+++ 9.DOM Manipulations and Events/05. Highlight-Active/app.js
@@ -1,3 +1,19 @@
 function focused() {
 
+    const inputRef = document.querySelectorAll("div div");
+
+    for(let input of inputRef){
+        input.children[1].addEventListener("focus", focusIventHandler);
+        input.children[1].addEventListener("blur", blurIventHandler);
+    }
+
+    function focusIventHandler(e){
+        e.target.parentElement.classList.add("focused");
+    }
+
+    function blurIventHandler(e){
+        e.target.parentElement.classList.remove("focused");
+    }
+
+
 }
--- 9.DOM Manipulations and Events/07. Shopping-Cart/solution.js
+++ 9.DOM Manipulations and Events/07. Shopping-Cart/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-   //TODO...
+   const addbuttonRef = document.querySelectorAll(".add-product");
+   const textArea = document.querySelector("textarea");
+   const checkButtonRef = document.querySelector(".checkout");
+   let list = [];
+   let totalCost = 0;
+
+   for(let addbutton of addbuttonRef){
+      addbutton.addEventListener("click", addbuttonHandler);
+   }
+
+   function addbuttonHandler(e){
+      const title = e.target.parentElement.parentElement.querySelector(".product-title").textContent;
+      const price = Number(e.target.parentElement.parentElement.querySelector(".product-line-price").textContent);
+      textArea.value += `Added ${title} for ${price.toFixed(2)} to the cart.\n`;
+      if(!list.includes(title)){
+         list.push(title);
+      }
+      totalCost += price;   
+   }
+
+   checkButtonRef.addEventListener("click", checkOutHandler);
+   function checkOutHandler(e) {
+      textArea.value += `You bought ${list.join(", ")} for ${totalCost.toFixed(2)}.`;
+      for(let button of addbuttonRef){
+         button.disabled = true;
+      }
+      checkButtonRef.disabled = true;
+   }
 }
